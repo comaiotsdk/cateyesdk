b676e6461db2b5800c8f703e15f7f8f107a12d37
路新星 <luxinxing@lianyun.tv>
2020-08-28 02:36:41 +0800

    更改判断是否有网络连接的条件

--- NetLibrary/src/main/java/com/comaiot/net/library/controller/AppUtils.java
+++ NetLibrary/src/main/java/com/comaiot/net/library/controller/AppUtils.java
@@ -3,28 +3,30 @@ package com.comaiot.net.library.controller;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.telephony.TelephonyManager;
-
-import static android.content.Context.TELEPHONY_SERVICE;
 
 @SuppressWarnings("all")
 public class AppUtils {
     public static boolean isHaveInternet(Context context) {
-        ConnectivityManager mConnectivity = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-        TelephonyManager mTelephony =
-                (TelephonyManager) context.getSystemService(TELEPHONY_SERVICE);   // 检查网络连接，如果无网络可用，就不需要进行连网操作等  
-        NetworkInfo info = mConnectivity.getActiveNetworkInfo();
-        if (info == null || !mConnectivity.getBackgroundDataSetting()) {
+        // 获取手机所有连接管理对象（包括对wi-fi,net等连接的管理）
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        if (connectivityManager == null) {
             return false;
-        }    //判断网络连接类型，只有在3G或wifi里进行一些数据更新。  
-        int netType = info.getType();
-        int netSubtype = info.getSubtype();
-        if (netType == ConnectivityManager.TYPE_WIFI) {
-            return info.isConnected();
-        } else if (netType == ConnectivityManager.TYPE_MOBILE && netSubtype == TelephonyManager.NETWORK_TYPE_UMTS && !mTelephony.isNetworkRoaming()) {
-            return info.isConnected();
         } else {
-            return false;
+            // 获取NetworkInfo对象
+            NetworkInfo[] networkInfo = connectivityManager.getAllNetworkInfo();
+
+            if (networkInfo != null && networkInfo.length > 0) {
+                for (int i = 0; i < networkInfo.length; i++) {
+                    System.out.println(i + "===状态===" + networkInfo[i].getState());
+                    System.out.println(i + "===类型===" + networkInfo[i].getTypeName());
+                    // 判断当前网络状态是否为连接状态
+                    if (networkInfo[i].getState() == NetworkInfo.State.CONNECTED) {
+                        return true;
+                    }
+                }
+            }
         }
+        return false;
     }
 }
